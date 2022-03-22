bd0ce581b6106023148edbaf46c64543c34bd625
ON Joseph <60672480+ONJoseph@users.noreply.github.com>
2022-03-22 01:12:57 +0100

    Update js index.js

--- js index.js
+++ js index.js
@@ -23,6 +23,5 @@ INSTRUCTIONS:
     ************/
 
 // PLEASE NOTE: Adding global style rules using the * selector, or by adding rules to body {..} or html {..}, or to all elements within body or html, i.e. h1 {..}, has the potential to pollute the test suite's CSS. Try adding: * { color: red }, for a quick example!
-
 // Once you have read the above messages, you can delete all comments. 
 "use strict";
